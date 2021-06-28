62ba9a4bf45f4d3981576f3cb47c9348e6031550
Jerich90 <83007935+Jerich90@users.noreply.github.com>
2021-06-28 02:18:54 +0200

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -9,10 +9,13 @@ links.forEach(function(element) {
 })
 
 function toggleMenu() {
+    body.classList.toggle('body--active-menu');
     hamburger.classList.toggle('hamburger--active');
     overlay.classList.toggle('overlay--active');
-    body.classList.toggle('body--active-menu');
 }
 
 
-hamburger.addEventListener('click', toggleMenu);
+hamburger.addEventListener('click', toggleMenu => {
+    toggleMenu.preventDefault();
+    overlay.classList.toggle('overlay--active');
+});
